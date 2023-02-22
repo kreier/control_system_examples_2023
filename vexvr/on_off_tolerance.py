351a0a024c65f340c8893b2e6ed2239c41a0cf0e
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-02-22 18:44:57 +0700

    updated for 2023

--- vexvr/on_off_tolerance.py
+++ vexvr/on_off_tolerance.py
@@ -1,47 +1,31 @@
+#region VEXcode Generated Robot Configuration
+import math
+import random
+from vexcode_vr import *
+
+# Brain should be defined by default
+brain = Brain()
+
+drivetrain = Drivetrain("drivetrain", 0)
+pen = Pen("pen", 8)
+pen.set_pen_width(THIN)
+left_bumper = Bumper("leftBumper", 2)
+right_bumper = Bumper("rightBumper", 3)
+front_eye = EyeSensor("frontEye", 4)
+down_eye = EyeSensor("downEye", 5)
+front_distance = Distance("frontdistance", 6)
+distance = front_distance
+magnet = Electromagnet("magnet", 7)
+location = Location("location", 9)
+#endregion VEXcode Generated Robot Configuration
 # ------------------------------------------
 # 
 # 	Project:      VEXcode Project
 #	Author:       VEX
-#	Created:
+#	Created:      2023/02/22
 #	Description:  VEXcode VR Python Project
 # 
 # ------------------------------------------
-
-# Library imports
-from vexcode import *
-
-
-drivetrain = Drivetrain()
-magnet = Electromagnet("magnet", 0)
-pen = Pen()
-brain = Brain()
-left_bumper = Bumper("leftBumper", 1)
-right_bumper = Bumper("rightBumper", 2)
-distance = Distance()
-front_eye = EyeSensor("fronteye", 3)
-down_eye = EyeSensor("downeye", 4)
-location = Location()
-
-
-def driveControlOnOff(duration):
-    setpoint = 0
-    speed = 100
-    # reset the timer
-    brain.timer_reset()
-    # begin algorithm to drive straight to the position of y = 0
-
-    # loop while the timer is less than the duration input of the function.
-    while(brain.timer_time(SECONDS)<duration):
-        currentYLocation = location.position(Y,MM)
-        if( currentYLocation < setpoint):
-            drivetrain.drive(FORWARD)
-        elif (currentYLocation > setpoint):
-            drivetrain.drive(REVERSE)
-        else:
-            drivetrain.stop()
-
-        #VEXCode VR requires that we have a small pause in any loop we run.    
-        wait(1,MSEC)
 
 def driveControlOnOffTolerance(duration):
     setpoint = 0
@@ -69,6 +53,5 @@ def main():
     driveControlOnOffTolerance(5)
     drivetrain.turn_to_heading(90, DEGREES)
 
-
 # VR threads — Do not delete
-vr_thread(main())
+vr_thread(main)
--- vexvr/prop_distance_parameter.py
+++ vexvr/prop_distance_parameter.py
@@ -1,27 +1,31 @@
+#region VEXcode Generated Robot Configuration
+import math
+import random
+from vexcode_vr import *
+
+# Brain should be defined by default
+brain = Brain()
+
+drivetrain = Drivetrain("drivetrain", 0)
+pen = Pen("pen", 8)
+pen.set_pen_width(THIN)
+left_bumper = Bumper("leftBumper", 2)
+right_bumper = Bumper("rightBumper", 3)
+front_eye = EyeSensor("frontEye", 4)
+down_eye = EyeSensor("downEye", 5)
+front_distance = Distance("frontdistance", 6)
+distance = front_distance
+magnet = Electromagnet("magnet", 7)
+location = Location("location", 9)
+#endregion VEXcode Generated Robot Configuration
 # ------------------------------------------
 # 
 # 	Project:      VEXcode Project
 #	Author:       VEX
-#	Created:
+#	Created:      2023/02/22
 #	Description:  VEXcode VR Python Project
 # 
 # ------------------------------------------
-
-# Library imports
-from vexcode import *
-
-
-drivetrain = Drivetrain()
-magnet = Electromagnet("magnet", 0)
-pen = Pen()
-brain = Brain()
-left_bumper = Bumper("leftBumper", 1)
-right_bumper = Bumper("rightBumper", 2)
-distance = Distance()
-front_eye = EyeSensor("fronteye", 3)
-down_eye = EyeSensor("downeye", 4)
-location = Location()
-
 
 def driveXDistance(setpoint,duration):
   
@@ -53,8 +57,8 @@ def driveXDistance(setpoint,duration):
 
 # Add project code in "main"
 def main():
-    
     drivetrain.turn_to_heading(90,DEGREES,wait=True)
-    driveXDistance(0,4)
+    driveXDistance(0,4)   
+
 # VR threads — Do not delete
 vr_thread(main())
